a24d81cef7719a278cbc3a771b6249b6f7e50bbb
Wstld <pontus.westlund@gmail.com>
2022-04-06 12:36:08 +0200

    color and BG added

--- lib/core/theme/colors.dart
+++ lib/core/theme/colors.dart
@@ -2,6 +2,9 @@ import 'package:flutter/material.dart';
 
 class ShColors {
   ShColors._();
-
-  static const black = Color.fromARGB(255, 0, 0, 0);
+  static const black = Color.fromRGBO(0, 0, 0, 1);
+  static const lightPink = Color.fromARGB(255, 255, 188, 188);
+  static const darkPink = Color.fromARGB(255, 243, 139, 160);
+  static const lightBlue = Color.fromARGB(255, 181, 234, 234);
+  static const lightGreen = Color.fromARGB(255, 237, 246, 229);
 }
