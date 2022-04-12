807e3c57c76d1cf33d0c18a3f25ad2e97d1a8a8c
Wstld <pontus.westlund@gmail.com>
2022-04-12 16:15:23 +0200

    Root Scroll bug fixed.

--- lib/presentation/ui/pages/home_root_page.dart
+++ lib/presentation/ui/pages/home_root_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:get/get.dart';
 import 'package:stable_helper/presentation/controller/home_root_controller.dart';
 import 'package:stable_helper/presentation/ui/ui.dart';
@@ -9,21 +10,21 @@ class HomeRootPage extends GetView {
 
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setEnabledSystemUIMode(SystemUiMode.immersiveSticky);
     return SafeArea(
       child: Scaffold(
         appBar: const ShAppBar(
           showBackBtn: false,
         ),
+        extendBody: true,
         drawer: const UserMainMenu(),
-        body: SingleChildScrollView(
-          child: GetBuilder<HomeRootController>(
-            init: Get.put(HomeRootController(Get.find(), Get.find())),
-            builder: (controller) => controller.obx(
-              (userData) => userData!.stablesId == null
-                  ? const UserHomeContainerNonMember()
-                  : const UserHomeContainerMember(),
-              onEmpty: const Text('Show on boarding'),
-            ),
+        body: GetBuilder<HomeRootController>(
+          init: Get.put(HomeRootController(Get.find(), Get.find())),
+          builder: (controller) => controller.obx(
+            (userData) => userData!.stablesId == null
+                ? const UserHomeContainerNonMember()
+                : const UserHomeContainerMember(),
+            onEmpty: const Text('Show on boarding'),
           ),
         ),
       ),
